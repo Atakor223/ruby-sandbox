79cbaae8c05b42e99e1060a7158d63d1b27745a5
Alex Takor <147947500+Atakor223@users.noreply.github.com>
2023-11-22 01:52:42 +0000

    finished

---
+++ howdy.rb
@@ -0,0 +1,18 @@
+# /Gemfile
+
+source "https://rubygems.org"
+
+gem "pry-byebug"
+
+require "active_support/all
+
+require "./goodbye.rb"
+
+# my_string = "Howdy there!"
+
+# pp my_string
+
+
+pp "What's your name?"
+
+their_name = gets.chomp
